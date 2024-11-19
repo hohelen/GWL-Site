22ec6b1b6465ada7e5dbeaf68964f7fc4d1fe422
tmei005 <tmei@ufl.edu>
2024-11-18 21:22:33 -0500

    calendar is clickable

--- Pages/Events/calendar.js
+++ Pages/Events/calendar.js
@@ -1,45 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: "dayGridMonth",
-      headerToolbar: {
-        center: 'addEventButton'
-      },
-      customButtons: {
-        addEventButton: {
-          text: 'Add Event...',
-          click: function() {
-            var title = prompt('Enter the event title');
-            if (title) {
-              var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-              if (dateStr) {
-                var timeStr = prompt('Enter a time in HH:MM format (24-hour clock)');
-                if (timeStr) {
-                  var dateTimeStr = dateStr + 'T' + timeStr + ':00'; // combine date and time
-                  var date = new Date(dateTimeStr); // will be in local time
-
-                  if (!isNaN(date.valueOf())) { // valid?
-                    calendar.addEvent({
-                      title: title,
-                      start: date,
-                      allDay: false
-                    });
-                    alert('Great. The event has been added to the calendar.');
-                  } else {
-                    alert('Invalid date/time.');
-                  }
-                } else {
-                  alert('Event time is required.');
-                }
-              } else {
-                alert('Event date is required.');
-              }
-            } else {
-              alert('Event title is required.');
-            }
-          }
-        }
-      }
-    });
-    calendar.render();
-});
+$(document).ready(function () {
+  $('#calendar').fullCalendar({
+    selectable: true,
+    selectHelper: true,
+    select: function()
+    {
+      $('#myModal').modal('toggle');
+    },
+    header:
+    {
+      left: 'month, agendaWeek, agendaDay, list',
+      center: 'title',
+      right: 'prev, today, next'
+    },
+    buttonText:
+    {
+      today: 'Today',
+      month: 'Month',
+      week: 'Week',
+      day: 'Day',
+      list: 'List'
+    }
+  });
+  });
--- Pages/Events/events.php
+++ Pages/Events/events.php
@@ -1,4 +1,16 @@
-<!DOCTYPE html>
+<!-- <-- <body>
+  <div id="header-container"></div>
+  <div class="page-title-container">
+    <h1>
+      <center>Events</center>
+    </h1>
+  </div>
+  <div id="calendar"></div>
+  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction/main.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.min.js"></script>
+  <div id="calendar"></div>
+</body> -->
 <html lang="en">
 
 <head>
@@ -8,14 +20,14 @@
   <link rel="stylesheet" href="../../Components/Overall/overall.css" />
   <link rel="stylesheet" href="../Header/header.css" />
   <link rel="stylesheet" href="../Footer/footer.css" />
-  <!-- FullCalendar Stylesheets -->
-  <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+
+  <title>Fullcalendar</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
-  <script src="calendar.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -25,23 +37,34 @@
       <center>Events</center>
     </h1>
   </div>
-  <!-- Calendar Container -->
-  <div id="calendar"></div>
-  <!-- FullCalendar Script -->
-  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction/main.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.min.js"></script>
-  <!-- Include separate JS file -->
-  <div id="calendar"></div>
-  <script src="../Events/calendar.js" defer></script>
-  <div id="footer-container"></div>
-  <script src="../Header/header.js" defer></script>
-  <script src="../Footer/footer.js" defer></script>
+  <div class="container">
+    <div id="calendar"></div>
+  </div>
+  <br>
 </body>
-<script>
-  $(document).ready(function () {
-    $('#calendar').fullCalendar();
-  });
-</script>
+
+</html>
+<script src="../Events/calendar.js" defer></script>
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Create Event</h4>
+      </div>
+      <div class="modal-body">
+        <input type="text" class="form-control">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="footer-container"></div>
+<script src="../Header/header.js" defer></script>
+<script src="../Footer/footer.js" defer></script>
+
 
 </html>
